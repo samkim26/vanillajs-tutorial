f301e71adb56a59fd9719e2af54b772c0e1f04ac
Myunghwan Kim <samkim2626@gmail.com>
2020-06-04 04:35:52 +0900

    Day 3: function

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-console.log('Im working');
+const title = document.querySelector('#title')
+// title.innerHTML = 'Hi! From JS'
+// title.style.color = 'red'
+// document.title = 'I own you now'
 
-let a = 221;
-let b = a - 5;
-a = 4; // variables with 'let' can be updated, with 'const' cannot be updated
-console.log(b, a);
+function handleClick() {
+  title.style.color = 'red'
+}
 
-// 변수 선언할 때 const로
+title.addEventListener('click', handleClick)
 
-const what = 'Nicolas';
-const wat = false; // false
-
-// Array
-const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-console.log(daysOfWeek[2]);
-
-// Object
-const nicoInfo = {
-  name: 'Nico',
-  age: 33,
-  gender: 'Male',
-  isHandsome: true,
-  favMovies: ['Along the gods', 'LOTR', 'Oldboy'],
-  favFood: [
-    {
-      name: 'Kimchi',
-      fatty: false
-    },
-    {
-      name: 'Cheese burger',
-      fatty: true
-    }
-  ]
-};
-console.log(nicoInfo);
+// https://codesandbox.io/s/day-three-blueprint-hook9?file=/src/index.js
