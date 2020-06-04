b3602b9121b87e72d7fb51988bd74f76a2a59625
Myunghwan Kim <samkim2626@gmail.com>
2020-06-05 02:07:11 +0900

    DAY 4: DOM with if-else

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const title = document.querySelector('#title')
 // title.innerHTML = 'Hi! From JS'
 // title.style.color = 'red'
@@ -10,3 +11,39 @@ function handleClick() {
 title.addEventListener('click', handleClick)
 
 // https://codesandbox.io/s/day-three-blueprint-hook9?file=/src/index.js
+*/
+
+/*
+// if-else
+if ('10' === 10) {
+  console.log('hi')
+} else if ('10' === '10') {
+  console.log('lalala')
+} else {
+  console.log('ho')
+}
+
+console.log((true || false) && false)
+
+const age = prompt('How old are u?')
+if (age > 18) {
+  console.log('you can drink')
+} else {
+  console.log('you cant')
+}
+*/
+
+// 이벤트는 MDN
+
+const title = document.querySelector('#title')
+
+const CLICKED_CLASS = 'clicked'
+
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS)
+}
+
+function init() {
+  title.addEventListener('click', handleClick)
+}
+init()
